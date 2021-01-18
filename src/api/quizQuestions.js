ec300234bd92e635239aed073089a3c56090d4e7
xshirl <xshirl525@gmail.com>
2021-01-18 16:04:51 -0500

    finished quiz

---
+++ src/api/quizQuestions.js
@@ -0,0 +1,88 @@
+var quizQuestions = [
+  {
+      question: "What trait do you value the most?",
+      answers: [
+          {
+              type: "Gryffindor",
+              content: "Courage"
+          },
+          {
+              type: "Ravenclaw",
+              content: "Wisdom"
+          },
+          {
+              type: "Hufflepuff",
+              content: "Loyalty"
+          },
+          {
+            type: "Slytherin",
+            content: "Ambition"
+        }
+      ]
+  },
+  {
+      question: "What would you like to do in your free time?",
+      answers: [
+          {
+            type: "Gryffindor",
+            content: "Going to a party"
+        },
+        {
+            type: "Ravenclaw",
+            content: "Reading a book"
+        },
+        {
+            type: "Hufflepuff",
+            content: "Hiking"
+        },
+        {
+          type: "Slytherin",
+          content: "Playing video games"
+      }
+      ]
+  },
+  {
+      question: "What is your favorite video game?",
+      answers: [
+        {
+          type: "Gryffindor",
+          content: "Fire emblem"
+      },
+      {
+          type: "Ravenclaw",
+          content: "Professor Layton"
+      },
+      {
+          type: "Hufflepuff",
+          content: "Harvest Moon"
+      },
+      {
+        type: "Slytherin",
+        content: "Phoenix Wright"
+    }
+      ]
+  },
+  {
+      question: "When do you die, you hope to...",
+      answers: [
+        {
+          type: "Gryffindor",
+          content: "Be surrounded by lifelong friends"
+      },
+      {
+          type: "Ravenclaw",
+          content: "Have learned everything there is to know"
+      },
+      {
+          type: "Hufflepuff",
+          content: "Be wealthy enough to provide for future generations"
+      },
+      {
+        type: "Slytherin",
+        content: "Have achieved success and travelled the world"
+    }
+      ]
+  }
+];
+
+export default quizQuestions;
